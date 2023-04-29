88432af3f4de74e3a289981e6a6c7663e806d375
justinwang <justinwang407@gmail.com>
2023-04-29 13:25:56 -0400

    Added basic camera

--- app/src/main/java/com/uottawa/felipemodesto/scoopscouter/MainActivity.java
+++ app/src/main/java/com/uottawa/felipemodesto/scoopscouter/MainActivity.java
@@ -1,7 +1,10 @@
 package com.uottawa.felipemodesto.scoopscouter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +13,20 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    ImageButton takePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
         mapFragment.getMapAsync(this);
+
+        takePicture = findViewById(R.id.reportSightingButton);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                        Manifest.permission.CAMERA
+                }, 100);
+            }
+        }
+
+        takePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
     }
 
     //triggered to add contents to map
@@ -35,5 +66,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 //.icon(BitmapDescriptorFactory.fromResource(R.drawable.icecream_marker))
         );
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+    }
+
+    private void openCamera(){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == 100){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        }
     }
 }
